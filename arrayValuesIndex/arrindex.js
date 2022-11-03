e77f7a604c9dc44aad7ed3c08702deb01401cd89
rajaadeeltalib <114896091+rajaadeeltalib@users.noreply.github.com>
2022-11-03 11:03:22 +0500

    Add files via upload

---
+++ arrayValuesIndex/arrindex.js
@@ -0,0 +1,19 @@
+//find missing values with index
+
+let array = [11, 15, 17, 18, 19];
+let array2 = array[0];
+let array3 = array[array.length -1];
+let i = 1;
+while (array2 < array3){
+  if(array[i] != ++array2){
+    array.splice(i, 0, array2);
+    i++;
+    console.log(`Number ${array2} at ${i-1}`);
+  }else{
+    i++;
+  }
+}
+console.log(array);
+
+
+
