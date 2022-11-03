cd950d5539831533d8fa1a52b6b5245a8f38a5c0
rajaadeeltalib <114896091+rajaadeeltalib@users.noreply.github.com>
2022-11-03 11:06:21 +0500

    Update arrindex.js

--- arrayValuesIndex/arrindex.js
+++ arrayValuesIndex/arrindex.js
@@ -8,12 +8,9 @@ while (array2 < array3){
   if(array[i] != ++array2){
     array.splice(i, 0, array2);
     i++;
-    console.log(`Number ${array2} at ${i-1}`);
+    console.log(`Number ${array2} index is ${i-1}`);
   }else{
     i++;
   }
 }
 console.log(array);
-
-
-
